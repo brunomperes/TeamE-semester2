b39901766462f3667283d6347e6949cf73ef2146
Gabrielius Mickevicius <gabrielius.m@gmail.com>
2014-02-10 16:38:52 +0000

    Build script. First component with an empty activator.

---
+++ src/team/e/components/users/Activator.java
@@ -0,0 +1,20 @@
+package team.e.components.users;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+public class Activator implements BundleActivator {
+	
+	@Override
+	public void start(BundleContext context)
+		throws Exception {
+		
+	}	
+
+	@Override
+	public void stop(BundleContext context)
+		throws Exception {
+		
+	}
+}
