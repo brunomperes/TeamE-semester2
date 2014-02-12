254262ae2fb0afc7fc35b63ee03363861043ddc4
Kronoser <christopher_meechan@hotmail.com>
2014-02-12 17:26:02 +0000

    Changed Mistakes :)

--- src/team/e/components/mycampus/stub/Activator.java
+++ src/team/e/components/mycampus/stub/Activator.java
@@ -4,22 +4,30 @@ import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
 
+import team.e.components.mycampus.IMyCampus;
+import team.e.components.users.auth.AuthResult;
+import team.e.components.users.auth.IAuthenticator;
+
 public class Activator implements BundleActivator{
 	
 	private MyCampusStub myCStub;
 	
-	private ServiceRegistration<MyCampusStub> myCampusStubRegistion;
+	private ServiceRegistration<IMyCampus> myCampusStubRegistion;
+	private ServiceRegistration<IAuthenticator> authResultRegistration;
 	
 	@Override
 	public void start(BundleContext context) throws Exception {
 		myCStub=new MyCampusStub();
-		myCampusStubRegistion=context.registerService(MyCampusStub.class, myCStub, null);
+		myCampusStubRegistion=context.registerService(IMyCampus.class, myCStub, null);
+		
+		authResultRegistration=context.registerService(IAuthenticator.class, myCStub, null);
 	}
 
 	@Override
 	public void stop(BundleContext arg0) throws Exception {
 		myCampusStubRegistion.unregister();
 		
+		authResultRegistration.unregister();
 	}
 
 }
