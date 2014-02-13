be6c41ba10c72b390be77fcf9ce48500c17b6bf3
Gabrielius Mickevicius <gabrielius.m@gmail.com>
2014-02-13 14:03:06 +0000

    MyCampus functions in LecturerAccess.

--- src/team/e/components/lecturer/LecturerAccess.java
+++ src/team/e/components/lecturer/LecturerAccess.java
@@ -1,6 +1,10 @@
 package team.e.components.lecturer;
 
+import java.util.Collection;
+
+import team.e.components.sysfunc.mycampus.MyCampusFunctions;
 import team.e.components.sysfunc.repository.IFunctionRepository;
+import team.e.components.sysfunc.timetable.Course;
 import team.e.components.sysfunc.timetable.CourseFunctions;
 import team.e.components.sysfunc.timetable.Session;
 import team.e.components.sysfunc.timetable.TimetableSlotFunctions;
@@ -12,5 +16,28 @@ public class LecturerAccess extends UserAccess {
 		CourseFunctions crFuncs = (CourseFunctions) funcRepo.getFunction(TimetableSlotFunctions.class);
 		
 		return crFuncs.addSession(session, courseID);
+	}
+	
+	public Collection<Course> getMyCampusCourses() {
+		IFunctionRepository funcRepo = getFuncRepo();
+		MyCampusFunctions mcFuncs = (MyCampusFunctions) funcRepo.getFunction(MyCampusFunctions.class);
+		
+		return mcFuncs.getCourseList();
+	}
+	
+	public boolean importMyCampusCourse(String courseID) {
+		IFunctionRepository funcRepo = getFuncRepo();
+		MyCampusFunctions mcFuncs = (MyCampusFunctions) funcRepo.getFunction(MyCampusFunctions.class);
+		CourseFunctions crFuncs = (CourseFunctions) funcRepo.getFunction(TimetableSlotFunctions.class);
+		
+		Collection<Course> courses = mcFuncs.getCourseList();
+		for (Course course: courses) {
+			if (course.getId().equals(courseID)) {
+				crFuncs.addCourse(course);
+				return true;
+			}
+		}
+		
+		return false;
 	}
 }
