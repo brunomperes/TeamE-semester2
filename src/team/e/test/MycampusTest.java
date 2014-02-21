13c14d5ac129db5a57cc052e368915c7388b1883
Adam Kurkiewicz <adam@kurkiewicz.pl>
2014-02-21 19:15:27 +0000

    added new dependency to ivy -- ant-junit. Created a mock test case. Changed the ant script to include the test cases. Now only needs more test cases. Before you run "ant test", please run "source setup-antjunit.sh"

---
+++ src/team/e/test/MycampusTest.java
@@ -0,0 +1,22 @@
+package team.e.test;
+import org.junit.Test;
+import org.junit.BeforeClass;
+import org.junit.AfterClass;
+import org.junit.Assert;
+
+public class MycampusTest{
+
+	@BeforeClass
+ 	public static void testSetup(){
+ 	}
+
+	@AfterClass 
+	public static void testCleanup(){
+	}
+
+	@Test
+	public void test1(){
+		Assert.assertTrue(true);
+	}
+}
+
