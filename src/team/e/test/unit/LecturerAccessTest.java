634b05dcff9bda0334484e750c81b064038b5c0b
Adam Kurkiewicz <adam@kurkiewicz.pl>
2014-02-27 15:53:44 +0000

    fix the ant script to work with new test cases, add the test xml to gitignore and delete a mock test case

--- src/team/e/test/unit/LecturerAccessTest.java
+++ src/team/e/test/unit/LecturerAccessTest.java
@@ -7,8 +7,6 @@ import team.e.components.lecturer.LecturerAccess;
 
 public class LecturerAccessTest {
 	
-	
-	//Test access to an empty list of courses. Test case seems to fail due to a null pointer exception.
 	@Test
 	public void testEmptyLecturerAccess() {
 		LecturerAccess lecacc = new LecturerAccess();
